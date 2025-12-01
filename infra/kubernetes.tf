98e5bbe436c80a64f35735d7d4fb47db1d26eb9e
Veselin Ivanov <me@veselyn.com>
2025-12-01 12:50:43 +0200

    feat(infra): provision non-ha kubernetes cluster

---
+++ infra/kubernetes.tf
@@ -0,0 +1,56 @@
+resource "proxmox_virtual_environment_vm" "kubernetes" {
+  for_each = local.kubernetes.nodes
+
+  name = each.key
+
+  vm_id     = each.value.vm_id
+  node_name = each.value.pve_node
+
+  clone {
+    vm_id = local.base_vm_id[each.value.pve_node]
+  }
+
+  cpu {
+    cores = 2
+  }
+
+  memory {
+    dedicated = 4096
+    floating  = 4096
+  }
+
+  disk {
+    interface    = "scsi0"
+    datastore_id = "local-zfs"
+    size         = 8
+  }
+
+  initialization {
+    datastore_id = "local-zfs"
+
+    dns {
+      servers = local.dns.servers
+    }
+
+    ip_config {
+      ipv4 {
+        address = each.value.ip_address
+        gateway = local.kubernetes.ip_gateway
+      }
+    }
+
+    user_account {
+      keys = [local.keys.ssh.master]
+    }
+  }
+}
+
+resource "cloudflare_dns_record" "kubernetes" {
+  for_each = local.kubernetes.nodes
+
+  zone_id = var.cloudflare_zone_id
+  name    = "${each.key}.kubernetes.homelab"
+  type    = "A"
+  content = split("/", each.value.ip_address)[0]
+  ttl     = 1
+}
--- infra/locals.tf
+++ infra/locals.tf
@@ -1,5 +1,37 @@
 locals {
-  sshkeys = <<-EOF
-    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJBKvLCEwUG+hT5G0PFIHLPJK/rM8EiPngEkAkvLmo22
-  EOF
+  keys = {
+    ssh = {
+      master = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJBKvLCEwUG+hT5G0PFIHLPJK/rM8EiPngEkAkvLmo22"
+    }
+  }
+
+  base_vm_id = {
+    pve01 = 9100
+    pve02 = 9200
+    pve03 = 9300
+    pve04 = 9400
+    pve05 = 9500
+  }
+
+  dns = {
+    servers = ["1.1.1.1", "1.0.0.1"]
+  }
+
+  kubernetes = {
+    ip_gateway = "10.42.1.1"
+
+    nodes = {
+      master01 = {
+        vm_id      = 101
+        pve_node   = "pve01"
+        ip_address = "10.42.1.21/24"
+      }
+
+      worker01 = {
+        vm_id      = 401
+        pve_node   = "pve04"
+        ip_address = "10.42.1.31/24"
+      }
+    }
+  }
 }
