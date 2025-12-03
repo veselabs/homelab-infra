48b8f46304eb6ccd18d2bba2d7b7d5e9f749e6a2
Veselin Ivanov <me@veselyn.com>
2025-12-03 19:07:23 +0200

    feat(config): provision non-ha kubernetes cluster (#13)
    * feat(infra): provision non-ha kubernetes cluster
    
    * feat(config): init kubespray git submodule at v2.29.0
    
    * feat(config): create inventories directory with pve
    
    * feat(config): create kubernetes inventory
    
    * feat(config): set upstream dns servers for k8s cluster
    
    * build(config): ignore kubernetes inventory variables lint
    
    * feat(config): provision ha kubernetes cluster
    
    * feat(infra): provision more resources for kubernetes
    
    * feat(kubernetes): deploy kube-vip for ha apiserver endpoint

---
+++ infra/kubernetes.tf
@@ -0,0 +1,64 @@
+resource "proxmox_virtual_environment_vm" "kubernetes" {
+  for_each = local.kubernetes.nodes
+
+  name      = each.key
+  node_name = each.value.pve_node
+  vm_id     = each.value.vm_id
+  tags      = ["kubernetes"]
+
+  clone {
+    vm_id = local.base_vm_id[each.value.pve_node]
+  }
+
+  cpu {
+    cores = 6
+  }
+
+  memory {
+    dedicated = 12288
+    floating  = 12288
+  }
+
+  disk {
+    interface    = "scsi0"
+    datastore_id = "local-zfs"
+    size         = 24
+  }
+
+  initialization {
+    datastore_id = "local-zfs"
+
+    dns {
+      servers = local.dns.servers
+    }
+
+    ip_config {
+      ipv4 {
+        address = each.value.ip_address
+        gateway = local.kubernetes.ip_gateway
+      }
+    }
+
+    user_account {
+      keys = [local.keys.ssh.master]
+    }
+  }
+}
+
+resource "cloudflare_dns_record" "kubernetes" {
+  for_each = local.kubernetes.nodes
+
+  zone_id = var.cloudflare_zone_id
+  name    = "${each.key}.kubernetes.homelab"
+  type    = "A"
+  content = split("/", each.value.ip_address)[0]
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "kubernetes_api" {
+  zone_id = var.cloudflare_zone_id
+  name    = "api.kubernetes.homelab"
+  type    = "A"
+  content = local.kubernetes.api_ip_address
+  ttl     = 1
+}
--- infra/locals.tf
+++ infra/locals.tf
@@ -1,5 +1,56 @@
 locals {
-  sshkeys = <<-EOF
-    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJBKvLCEwUG+hT5G0PFIHLPJK/rM8EiPngEkAkvLmo22
-  EOF
+  keys = {
+    ssh = {
+      master = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJBKvLCEwUG+hT5G0PFIHLPJK/rM8EiPngEkAkvLmo22"
+    }
+  }
+
+  base_vm_id = {
+    pve01 = 9100
+    pve02 = 9200
+    pve03 = 9300
+    pve04 = 9400
+    pve05 = 9500
+  }
+
+  dns = {
+    servers = ["1.1.1.1", "1.0.0.1"]
+  }
+
+  kubernetes = {
+    ip_gateway     = "10.42.1.1"
+    api_ip_address = "10.42.1.20"
+
+    nodes = {
+      master01 = {
+        vm_id      = 111
+        pve_node   = "pve01"
+        ip_address = "10.42.1.21/24"
+      }
+
+      master02 = {
+        vm_id      = 211
+        pve_node   = "pve02"
+        ip_address = "10.42.1.22/24"
+      }
+
+      master03 = {
+        vm_id      = 311
+        pve_node   = "pve03"
+        ip_address = "10.42.1.23/24"
+      }
+
+      worker01 = {
+        vm_id      = 421
+        pve_node   = "pve04"
+        ip_address = "10.42.1.24/24"
+      }
+
+      worker02 = {
+        vm_id      = 521
+        pve_node   = "pve05"
+        ip_address = "10.42.1.25/24"
+      }
+    }
+  }
 }
